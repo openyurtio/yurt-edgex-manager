e29f24b71d21b80a5ab3f3bd160f3730536f0136
huiwq1990 <huiwq1990@163.com>
2022-05-14 13:47:10 +0800

    change go module name (#16)

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -30,7 +30,7 @@ import (
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
-	devicev1alpha1 "github.com/lwmqwer/EdgeX/api/v1alpha1"
+	devicev1alpha1 "github.com/openyurtio/yurt-edgex-manager/api/v1alpha1"
 	//+kubebuilder:scaffold:imports
 )
 
